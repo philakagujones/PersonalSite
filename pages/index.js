e37c98ea28a5af65ab05211a24e21f79d11a913b
philakagujones <philakagujones@gmail.com>
2022-07-04 22:04:40 -0700

    small design changes + links

--- pages/index.js
+++ pages/index.js
@@ -26,9 +26,9 @@ export default function Home() {
         <div className={styles.message}>
           <p>
             Hello my name is Phil. I am currently in my second year at Simon Fraser University pursuing a degree in computer science. 
-            I was introduced to web development in 2018 and in high school I cofounded a creative agency, <a href='www.studiopluto.co' className='text-stone-500'>Studio Pluto</a>, which specializes in the creation of websites. 
+            I was introduced to web development in 2018 and in high school I cofounded a creative agency, <a href={'https://www.studiopluto.co'} target={'_blank'} rel={'noopener noreferrer'} className='text-stone-500 hover:text-stone-400'>Studio Pluto</a>, which specializes in the creation of websites. 
             As of late, I've been given the privelege of working at an indie game studio so these days I spend most of my time playing around in Unity and with C#. 
-            In my free time, I work on <a href='www.drabble.app' className='text-stone-500'>Drabble</a>, make designs or create small automation scripts to help simplify life. <br /> 🤎
+            In my free time, I work on <a href={'https://www.drabble.app'} target={'_blank'} rel={'noopener noreferrer'} className='text-stone-500 hover:text-stone-400'>Drabble</a>, make designs or create small automation scripts to help simplify life. <br /> 🤎
           </p>
         </div>
 
