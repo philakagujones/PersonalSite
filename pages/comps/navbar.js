ce1c42ec00407a276c40d0073fd178fd28aaf98c
philakagujones <philakagujones@gmail.com>
2025-11-08 08:55:09 -0800

    Update navbar.js

--- pages/comps/navbar.js
+++ pages/comps/navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className={styles.navwrapper}>
       <div className={styles.navleft}>
-          <div className={styles["navname"] + styles["hoveranimation"]}>phil akagu-jones</div>
+          <div className={styles["navname"] + styles["hoveranimation"]}>phil jones</div>
           <Toggle />
       </div>
       <div className={styles.navright}>
